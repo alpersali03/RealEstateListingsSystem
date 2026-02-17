c58b1852910e5d580e21752e49bbf2692e8ce72d
Sigma98 <alsalilee@gmail.com>
2026-02-17 14:14:53 +0200

    Add PropertyTypesController
    Introduce PropertyTypesController with an Index action that uses RealEstateDbContext to load and return PropertyTypes. Update .gitignore to include new FileContentIndex entries and persist IDE workspace changes (.vs DocumentLayout, .suo, CopilotIndices). Rebuild artifacts (dll, exe, pdb) were also updated.

---
+++ RealEstateListingsSystem/Controllers/PropertyTypesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateListingsSystem.Data;
+
+namespace RealEstateListingsSystem.Controllers
+{
+	public class PropertyTypesController : Controller
+	{
+		private readonly RealEstateDbContext _context;
+		public PropertyTypesController(RealEstateDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			var propertyTypes = _context.PropertyTypes.ToList();	
+			return View(propertyTypes);
+		}
+	}
+}
